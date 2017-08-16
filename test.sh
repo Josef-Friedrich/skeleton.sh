fd6b59af0bbc79ec67bd2f085923054f93087d1e
Josef Friedrich <josef@friedrich.rocks>
2017-08-16 21:47:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,27 @@
 #! /bin/sh
 
 # bash_unit
-echo "
+if ls test/*.bash_unit > /dev/null 2>&1; then
+	echo "
 Running bash_unit tests:"
-./test/lib/bash_unit test/*.bash_unit
-RETURN_BASH_UNIT=$?
+	./test/lib/bash_unit test/*.bash_unit
+	RETURN_BASH_UNIT=$?
+else
+	RETURN_BASH_UNIT=0
+fi
 
 # bats
-echo "
+if ls test/*.bats > /dev/null 2>&1; then
+	echo "
 Running bats tests:"
-./test/lib/bats/bats test
-## or:
-# bats test
-RETURN_BATS=$?
+	./test/lib/bats/bats test
+	## or:
+	# bats test
+	RETURN_BATS=$?
+else
+	RETURN_BATS=0
+fi
+
 
 if [ 0 -eq "$RETURN_BASH_UNIT" ] && [ 0 -eq "$RETURN_BATS" ] ; then
 	echo 'All tests pass!'
