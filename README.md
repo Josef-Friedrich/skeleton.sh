2a9db58ea258790259ed666a665c35b02c417a19
Josef Friedrich <josef@friedrich.rocks>
2017-08-17 13:51:37 +0200

    Render README

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 [![Build Status](https://travis-ci.org/JosefFriedrich-shell/skeleton.svg?branch=master)](https://travis-ci.org/JosefFriedrich-shell/skeleton)
 
-# Project name
+# skeleton
+
+## Usage
+
+```
+Usage: This is a example script
+
+Options:
+	- h: Show this help message
+```
 
 ## Testing
 
--- sync-skeleton.sh
+++ sync-skeleton.sh
@@ -5,26 +5,31 @@ _get() {
 	wget -O "$1" "https://raw.githubusercontent.com/JosefFriedrich-shell/skeleton/master/$1"
 }
 
+_getx() {
+	_get "$1"
+	chmod a+x "$1"
+}
+
+_getif() {
+	if [ ! -f "$1" ]; then
+		_get "$1"
+	fi
+}
+
 ## This SEPARATOR is required for test purposes. Please don’t remove! ##
 
 _get .travis.yml
 _get Makefile
-_get test.sh
-_get test/lib/bash_unit
-_get test/lib/bats/bats
-_get test/lib/bats/bats-exec-suite
-_get test/lib/bats/bats-exec-test
-_get test/lib/bats/bats-format-tap-stream
-_get test/lib/bats/bats-preprocess
 _get test/lib/test-helper.sh
-
-chmod a+x test.sh
-chmod a+x test/lib/bash_unit
-chmod a+x test/lib/bats/bats
-chmod a+x test/lib/bats/bats-exec-suite
-chmod a+x test/lib/bats/bats-exec-test
-chmod a+x test/lib/bats/bats-format-tap-stream
-chmod a+x test/lib/bats/bats-preprocess
+_getx test.sh
+_getx test/lib/bash_unit
+_getx test/lib/bats/bats
+_getx test/lib/bats/bats-exec-suite
+_getx test/lib/bats/bats-exec-test
+_getx test/lib/bats/bats-format-tap-stream
+_getx test/lib/bats/bats-preprocess
+_getx test/lib/render-readme.sh
+_getif README.md.template.sh
 
 rm -f test/bash_unit
 rm -f test/test-helper.sh
