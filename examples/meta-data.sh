a1d2cc45084f30a76e21fdfb8b29c0ecc2f4fd75
Josef Friedrich <josef@friedrich.rocks>
2017-08-18 19:47:31 +0200

    Add new example

---
+++ examples/meta-data.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+FIRST_RELEASE=2017-08-13
+PROJECT_PAGES="https://github.com/JosefFriedrich-shell/skeleton"
+SHORT_DESCRIPTION='This is an example script to demonstrate the metadata usage in shell scripts!'
+USAGE="Usage: $(basename "$0") [-hsv]
+
+"
+VERSION=1.0
