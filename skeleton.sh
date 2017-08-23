4fa9368fd3c49267e5d579222cd408bec0f8e042
Josef Friedrich <josef@friedrich.rocks>
2017-08-23 11:15:00 +0200

    Combine all scripts into one

--- skeleton.sh
+++ skeleton.sh
@@ -36,11 +36,15 @@ $SHORT_DESCRIPTION
 Options:
 	-h, --help
 	  Show this help message
+	-r, --render-readme
+	  Render “README.md”.
 	-s, --short-description
 	  Show a short description / summary
 	-v, --version
 	  Show the version number of this script.
 "
+PROJECT_NAME="$(basename "$(pwd)")"
+
 
 # See https://stackoverflow.com/a/28466267
 
@@ -49,7 +53,7 @@ Options:
 # Missing argument: 3
 # No argument allowed: 4
 _getopts() {
-	while getopts ':ab:chsv-:' OPT ; do
+	while getopts ':ab:chrsv-:' OPT ; do
 		case $OPT in
 			a)
 				OPT_ALPHA=1
@@ -64,6 +68,7 @@ _getopts() {
 				;;
 
 			h) echo "$USAGE"; exit 0;;
+			r) OPT_README=1;;
 			s) echo "$SHORT_DESCRIPTION"; exit 0;;
 			v) echo "$VERSION"; exit 0;;
 
@@ -89,8 +94,6 @@ _getopts() {
 						OPT_BRAVO="$LONG_OPTARG"
 						;;
 
-
-
 					charlie)
 						OPT_CHARLIE=1
 						;;
@@ -106,6 +109,7 @@ _getopts() {
 						;;
 
 					help) echo "$USAGE"; exit 0;;
+					render-readme) OPT_README=1;;
 					short-description) echo "$SHORT_DESCRIPTION"; exit 0;;
 					version) echo "$VERSION"; exit 0;;
 
@@ -131,6 +135,120 @@ _getopts() {
 	GETOPTS_SHIFT=$((OPTIND - 1))
 }
 
+_sync_skeleton() {
+	_get() {
+		mkdir -p "$(dirname "$1")"
+		wget -O "$1" "https://raw.githubusercontent.com/JosefFriedrich-shell/skeleton/master/$1"
+	}
+
+	_getx() {
+		_get "$1"
+		chmod a+x "$1"
+	}
+
+	_getif() {
+		if [ ! -f "$1" ]; then
+			_get "$1"
+		fi
+	}
+
+	_rm() {
+		rm -rf "$1"
+	}
+
+
+	_get .travis.yml
+	_get Makefile
+	_get test/lib/test-helper.sh
+	_getif LICENSE
+	_getx test/lib/bash_unit
+	_getx test/lib/bats/bats
+	_getx test/lib/bats/bats-exec-suite
+	_getx test/lib/bats/bats-exec-test
+	_getx test/lib/bats/bats-format-tap-stream
+	_getx test/lib/bats/bats-preprocess
+	_getx test/lib/render-readme.sh
+	_getx test/lib/test-runner.sh
+
+	_rm README.md.template.sh
+	_rm sync-skeleton.sh
+	_rm test.sh
+	_rm test/bash_unit
+	_rm test/test-helper.sh
+}
+
+_sync_dependencies() {
+	_get() {
+		mkdir -p "$(dirname "$1")"
+		wget -O "$1" "https://raw.githubusercontent.com/$2"
+	}
+
+
+	_get test/lib/bash_unit pgrange/bash_unit/master/bash_unit
+	_get test/lib/test-helper.sh JosefFriedrich-shell/test-helper.sh/master/test-helper.sh
+
+	rm -rf tmp_bats
+	rm -rf test/lib/bats
+	git clone https://github.com/sstephenson/bats.git tmp_bats
+	mv tmp_bats/libexec test/lib/bats
+	rm -rf tmp_bats
+}
+
+_render_readme() {
+
+	. ./test/lib/test-helper.sh
+
+	source_exec ./$PROJECT_NAME
+
+	> README.md
+
+	########################################################################
+
+	cat <<EOF >> README.md
+[![Build Status](https://travis-ci.org/JosefFriedrich-shell/$PROJECT_NAME.svg?branch=master)](https://travis-ci.org/JosefFriedrich-shell/$PROJECT_NAME)
+
+# $PROJECT_NAME
+EOF
+
+	###### README HEADER ###########################################
+
+	echo >> README.md
+	[ -f README-header.md ] && cat README-header.md >> README.md
+	echo >> README.md
+
+	### SHORT_DESCRIPTION ##########################################
+
+	echo '## Summary / Short description' >> README.md
+	echo >> README.md
+	echo "> $SHORT_DESCRIPTION" >> README.md
+	echo >> README.md
+
+	### USAGE ######################################################
+
+	cat <<'EOF' >> README.md
+## Usage
+
+```
+EOF
+	echo "$USAGE" >> README.md
+	echo '```'  >> README.md
+
+	### TESTING ####################################################
+
+	cat <<'EOF' >> README.md
+## Testing
+
+```
+make test
+```
+EOF
+
+	### README FOOTER ##############################################
+
+	echo >> README.md
+	[ -f README-footer.md ] && cat README-footer.md >> README.md
+}
+
 ## This SEPARATOR is required for test purposes. Please don’t remove! ##
 
 _getopts $@
@@ -138,6 +256,8 @@ shift $GETOPTS_SHIFT
 
 [ -n "$1" ] && echo "Parameter 1: $1"
 [ -n "$2" ] && echo "Parameter 2: $2"
+
+[ "$OPT_README" -eq 1 ] && _render_readme
 
 cat <<EOF
       .-.
